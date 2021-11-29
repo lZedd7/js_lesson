25e1d36581d3c2c14ae499c193002934aa8e5b04
Alex Sydorenko <alexsydorenko7r@gmail.com>
2021-11-29 17:18:30 +0200

    finished 2nd lesson

--- Lesson_2/scrpit.js
+++ Lesson_2/scrpit.js
@@ -1,2 +1,36 @@
-let name = prompt('whats your name');
-alert(`hello ${name}`+`,how are you`);
+let operator = getOperation();
+
+const operandA = getOperand('A');
+const operandB = getOperand('B');
+
+const result = calculate(operator, operandA, operandB);
+
+showResult(operator, operandA, operandB,result);
+
+function getOperand(operandName){
+  return  Number(prompt("Operand"+ operandName));
+}
+function getOperation(){
+    return prompt('what to do ');
+}
+
+
+function calculate(op,a,b){
+    let result;
+
+switch(op){ 
+    case '+' : result= a + b; break
+    case '-' : result= a - b; break
+    case '/' : result= a / b; break
+    case '*' : result= a * b; break
+    }
+    return result; 
+}
+
+function showResult(op,a,b,res){
+    console.log(`${a} ${op} ${b} = ${res}`);
+}
+
+
+
+
