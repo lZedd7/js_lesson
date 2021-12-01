fe29945cd85ea472ef50cc85efa033e8f10ae21c
Alex Sydorenko <alexsydorenko7r@gmail.com>
2021-12-01 23:54:18 +0200

    finished 3 lesson

--- Lesson_3/scrpit.js
+++ Lesson_3/scrpit.js
@@ -1,8 +1,40 @@
+let operator = getOperation();
 
+const operandA = getOperand('A');
+const operandB = getOperand('B');
 
-const arr  = [1,3,5,7,-1,-8];
-console.log();
+const result = calculate(operator, operandA, operandB);
 
+showResult(operator, operandA, operandB,result);
 
+function getOperand(operandName){
+    let operand 
+    do {
+        operand = Number(prompt("Operand"+ operandName));
+    } while (isNaN(operand))
+    operand =getOperand('A');
+    return operand;
+}
+function getOperation(){
+    let operator;
+do  { 
+    operator = prompt('what to do ');
+} while (!(operator === '+' || operator === '-' || operator === '/' || operator === '*'))
+ return operator;
+} 
 
+function calculate(op,a,b){
+    let result;
 
+switch(op){ 
+    case '+' : result= a + b; break
+    case '-' : result= a - b; break
+    case '/' : result= a / b; break
+    case '*' : result= a * b; break
+    }
+    return result; 
+}
+
+function showResult(op,a,b,res){
+    console.log(`${a} ${op} ${b} = ${res}`);
+}
